9994c6ff954d5107c7198447e60bea4c82c7be0d
tslumley <t.lumley@auckland.ac.nz>
2020-12-09 11:08:17 +1300

    first complete draft

--- bpower/app.R
+++ bpower/app.R
@@ -10,6 +10,10 @@ library(shinythemes)
 
 library(shiny)
 
+magic_power_number<-function(halfalpha,beta){
+    (qnorm(halfalpha)+qnorm(beta))^2
+}
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
@@ -23,6 +27,8 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
             sidebarLayout(
             sidebarPanel(
+                helpText("This panel sets the effect size distribution (mortality reduction) across trials, for the subset of patients who could realistically benefit from the intervention. Our prior expectation is that this is mostly non-zero, but rarely more than a 10% mortality reduction"),
+                br(),
                 sliderInput("pnull",
                         "Percentage null:",
                         min = 0,
@@ -31,7 +37,7 @@ ui <- fluidPage(
                 hr(),
                 helpText("Adjust these so the graph matches your prior"),
                 sliderInput("beta_a","Beta(a,)",min=1,max=10,value=2),
-                sliderInput("beta_b","Beta(,b)",min=1,max=10,value=4)
+                sliderInput("beta_b","Beta(,b)",min=1,max=10,value=6)
             ),
 
         # Show a plot of the generated distribution
@@ -48,11 +54,14 @@ ui <- fluidPage(
                  
                  # Sidebar with a slider input for number of bins 
                  sidebarLayout(
+                     
                      sidebarPanel(
-
+                         helpText("This panel sets the distribution across trials of the proportion of patients who could realistically benefit from the intervention. Our prior expectation is that this is typically about 20%, but could sometimes be larger"),
+                         br(),
+                         hr(),
                          helpText("Adjust these so the graph matches your prior"),
                          sliderInput("treat_a","Beta(a,)",min=1,max=50,value=10),
-                         sliderInput("treat_b","Beta(,b)",min=1,max=50,value=30)
+                         sliderInput("treat_b","Beta(,b)",min=1,max=50,value=40)
                      ),
                      
                      # Show a plot of the generated distribution
@@ -65,15 +74,17 @@ ui <- fluidPage(
              # Application title
              sidebarLayout(
                  titlePanel("True treatment effect in all-comers"),
-                 
                  # Sidebar with a slider input for number of bins 
                  sidebarLayout(
                      sidebarPanel(
+                         helpText("This panel sets the typical power for trials, in terms of the effect size in all-comers. For example, you might think it's typical to have 90% power for a 10% reduction in mortality, or that it's conservative to try 80% power for a 5% reduction."),
+                         hr(),
+                         
                          radioButtons("power", "Power:",
                                       c("80%" = .8,
                                         "85%" = .85,
                                         "90%" = .9)),
-                         sliderInput("power_at","at all-comers mortality reduction of:",min=1,max=20,value=4)
+                         sliderInput("power_at","at all-comers mortality reduction of:",min=1,max=20,value=5)
                          
                      ),
                      
@@ -82,8 +93,8 @@ ui <- fluidPage(
                          plotOutput("nettreatPlot"),
                          hr(),
                          br(),
-                         h4(paste("Expected fraction of positive trials:"),
-                            textOutput("fraction_positive"))
+                         h4(p("Combining the assumptions about treatment effect distribution, fraction treatable, and typical design power, we would expect to see")),
+                         h4(textOutput("fraction_positive",inline=TRUE))
                      )
                  )
              )
@@ -112,19 +123,19 @@ server <- function(input, output) {
         # generate bins based on input$bins from ui.R
         xgrid<-seq(0,1,length=101)
         ygrid<-dbeta(xgrid,input$treat_a,input$treat_b)
-        curve(dbeta(x,input$treat_a,input$treat_b),from=0,to=1,xlab="Proportion treatable")
+        curve(dbeta(x,input$treat_a,input$treat_b),from=0,to=1,xlab="Proportion treatable",ylab="Density")
         polygon(c(xgrid,0),c(ygrid,0),col="blue")
 
     })
     
     output$nettreatPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
         x <- rbeta(10000,input$beta_a,input$beta_b)*20
         x[runif(10000)<(input$pnull/100)]<-0
         y<-rbeta(10000,input$treat_a,input$treat_b)
         z<-x*y
         hist(z,breaks = (0:20), col = 'purple', border = 'white',prob=TRUE,xlab="Net treatment effect (% mortality reduction)",
              main="" )
+        abline(v=input$power_at,lty=2)
         
     })
     
@@ -133,8 +144,11 @@ server <- function(input, output) {
         x[runif(10000)<(input$pnull/100)]<-0
         y<-rbeta(10000,input$treat_a,input$treat_b)
         d<-x*y
-        "Need formula"       
-        
+        zthing<-sqrt(magic_power_number(0.025, 1-as.numeric(input$power))*(d/input$power_at))
+        zbeta<-zthing+qnorm(0.025)
+        power<-pnorm(zthing+qnorm(0.025))
+        power_wrong_tail<-pnorm(zthing-qnorm(0.025),lower.tail=FALSE)
+        paste("% of positive trials =", round(100*mean((power+power_wrong_tail))))
     })
 }
 
